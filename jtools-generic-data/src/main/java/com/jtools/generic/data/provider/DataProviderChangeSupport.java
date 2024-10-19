e024252c5b83d6f14cc823485793098e52ec6575
j4ckk0 <j4ckk0@jackk0.fr>
2024-10-19 18:10:41 +0200

    Add properties to send properties changes : "data provider added" + "data provider removed"

--- jtools-generic-data/src/main/java/com/jtools/generic/data/provider/DataProviderChangeSupport.java
+++ jtools-generic-data/src/main/java/com/jtools/generic/data/provider/DataProviderChangeSupport.java
@@ -9,5 +9,9 @@ package com.jtools.generic.data.provider;
  */
 public interface DataProviderChangeSupport {
 
+	public static final String DATA_PROVIDER_ADDED_PROPERTY = "DATA_PROVIDER_ADDED_PROPERTY";
+
+	public static final String DATA_PROVIDER_REMOVED_PROPERTY = "DATA_PROVIDER_REMOVED_PROPERTY";
+	
 	public static final String DATA_PROVIDER_CHANGED_PROPERTY = "DATA_PROVIDER_CHANGED_PROPERTY";
 }
